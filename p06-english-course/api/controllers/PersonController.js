500a853a1ed86c9011d6e1de68dc8111bcdb81da
Mariana <marianaufc@gmail.com>
2022-04-14 15:20:02 -0300

    feat(p06): Creates CRUD for People

--- p06-english-course/api/controllers/PersonController.js
+++ p06-english-course/api/controllers/PersonController.js
@@ -1,10 +1,52 @@
 const db = require('../models');
 
 class PersonController {
-    static async listAll(req, res) {
+    static async getAll(req, res) {
         try {
             const allPeople = await db.People.findAll();
             return res.status(200).json(allPeople);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async getById(req, res) {
+        const { id } = req.params;
+        try {
+            const person = await db.People.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(person);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async create(req, res) {
+        const newPerson = req.body;
+        try {
+            const person = await db.People.create(newPerson);
+            return res.status(200).json(person);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async update(req, res) {
+        const { id } = req.params;
+        const newValues = req.body;
+        try {
+            await db.People.update(newValues, { where: { id: Number(id) } });
+            const person = await db.People.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(person);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async delete(req, res) {
+        const { id } = req.params;
+        try {
+            await db.People.destroy({ where: { id: Number(id) } });
+            return res.status(200).json({ message: `Registro com id ${id} apagado` });
         } catch (error) {
             return res.status(500).json(error.message);
         }
--- p06-english-course/api/routes/peopleRoute.js
+++ p06-english-course/api/routes/peopleRoute.js
@@ -3,6 +3,10 @@ const PersonController = require('../controllers/PersonController');
 
 const router = Router();
 
-router.get('/pessoas', PersonController.listAll);
+router.get('/pessoas', PersonController.getAll);
+router.get('/pessoas/:id', PersonController.getById);
+router.post('/pessoas', PersonController.create);
+router.put('/pessoas/:id', PersonController.update);
+router.delete('/pessoas/:id', PersonController.delete);
 
 module.exports = router;
