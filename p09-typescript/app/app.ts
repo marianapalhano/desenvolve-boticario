58c7839f21d5914f391b2d6023d6ba01e1c4c460
Mariana <marianaufc@gmail.com>
2022-06-03 18:12:12 -0300

    feat(p09): creates negociacao controller

--- p09-typescript/app/app.ts
+++ p09-typescript/app/app.ts
@@ -1,3 +1,9 @@
-import { Negociacao } from './models/negociacao.js';
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+    controller.adiciona();
+})
