850bb78191b7fdba606fde512a5ee9f4e4fdae68
Mariana <marianaufc@gmail.com>
2022-06-10 17:40:51 -0300

    feat(p09): code refactoring

--- p09-typescript/app/controllers/negociacao-controller.ts
+++ p09-typescript/app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes: Negociacoes = new Negociacoes;
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -13,6 +15,9 @@ export class NegociacaoController {
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
+        this.negociacoes.adiciona(negociacao);
+        this.negociacoes.lista();
+        this.limparFormulario();
     }
 
     criaNegociacao(): Negociacao {
